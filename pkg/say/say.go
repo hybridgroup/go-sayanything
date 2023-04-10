f9dd84dd2d5915fbd15b1be33b6233819295efa4
deadprogram <ron@hybridgroup.com>
2023-04-10 13:42:28 +0200

    pkg/say: add initial support for oto based audio output
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ pkg/say/say.go
@@ -0,0 +1,36 @@
+package say
+
+import (
+	"github.com/hajimehoshi/oto"
+	"github.com/tosone/minimp3"
+)
+
+type Player struct {
+	player *oto.Player
+}
+
+func NewPlayer() *Player {
+	return &Player{}
+}
+
+func (p *Player) Close() {
+	if p.player != nil {
+		p.player.Close()
+		p.player = nil
+	}
+}
+
+func (p *Player) Say(b []byte) error {
+	dec, data, err := minimp3.DecodeFull(b)
+	if err != nil {
+		return err
+	}
+
+	if p.player == nil {
+		player, _ := oto.NewContext(dec.SampleRate, dec.Channels, 2, 1024)
+		p.player = player.NewPlayer()
+	}
+
+	_, err = p.player.Write(data)
+	return err
+}
